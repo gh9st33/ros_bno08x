4024e40fdef57e05a0c2a36ac5c045211cba2746
Sourcery AI <>
2023-06-24 02:22:03 +0000

    'Refactored by Sourcery'

--- src/talker.py
+++ src/talker.py
@@ -25,7 +25,7 @@ def bno08x_node():
     status_pub = rospy.Publisher('bno08x/status', DiagnosticStatus, queue_size=10)
     rospy.init_node('bno08x')
     rate = rospy.Rate(100) # frequency in Hz
-    rospy.loginfo(rospy.get_caller_id() + "  bno08x node launched.")
+    rospy.loginfo(f"{rospy.get_caller_id()}  bno08x node launched.")
 
     i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
     bno = BNO08X_I2C(i2c,address=0x4b) # BNO080 (0x4b) BNO085 (0x4a)
@@ -34,7 +34,7 @@ def bno08x_node():
     bno.enable_feature(BNO_REPORT_GYROSCOPE)
     bno.enable_feature(BNO_REPORT_MAGNETOMETER)
     bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
-    
+
     time.sleep(0.5) # ensure IMU is initialized
 
     while True:
@@ -50,14 +50,14 @@ def bno08x_node():
         raw_msg.angular_velocity.x = gyro_x
         raw_msg.angular_velocity.y = gyro_y
         raw_msg.angular_velocity.z = gyro_z
-        
+
         quat_i, quat_j, quat_k, quat_real = bno.quaternion
         raw_msg.orientation.w = quat_i
         raw_msg.orientation.x = quat_j
         raw_msg.orientation.y = quat_k
         raw_msg.orientation.z = quat_real
 
-        
+
         raw_msg.orientation_covariance[0] = -1
         raw_msg.linear_acceleration_covariance[0] = -1
         raw_msg.angular_velocity_covariance[0] = -1
@@ -72,7 +72,7 @@ def bno08x_node():
         mag_msg.magnetic_field.z = mag_z
         mag_msg.magnetic_field_covariance[0] = -1
         mag_pub.publish(mag_msg)
-        
+
         status_msg = DiagnosticStatus()
         status_msg.level = 0
         status_msg.name = "bno08x IMU"
@@ -80,11 +80,11 @@ def bno08x_node():
         status_pub.publish(status_msg)
 
         rate.sleep()   
-    
-    rospy.loginfo(rospy.get_caller_id() + "  bno08x node finished")
+
+    rospy.loginfo(f"{rospy.get_caller_id()}  bno08x node finished")
 
 if __name__ == '__main__':
     try:
         bno08x_node()
     except rospy.ROSInterruptException:
-        rospy.loginfo(rospy.get_caller_id() + "  bno08x node exited with exception.")
+        rospy.loginfo(f"{rospy.get_caller_id()}  bno08x node exited with exception.")
